cba4759bdaf1316dfcaf841080d47bd996249817
Nikola <tomaha15@gmail.com>
2022-12-08 03:01:13 +0700

    Create new book function
    Style backdrop
    Postition all elements

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let addTitleBtn = document.getElementById('new-title');
+let newEntry = document.getElementById('new-entry');
+
+let gridCon = document.getElementById('book-grid');
+
+let closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', () => {
+    closeBtn.classList.toggle('active');
+    newEntry.classList.toggle('displayToggle');
+
+});
+
+addTitleBtn.addEventListener('click', () => {
+    newEntry.classList.toggle('displayToggle');
+    closeBtn.classList.toggle('active');
+} );
+
+let newBook = document.getElementById('new-book');
+newBook.addEventListener('click', () => {
+    
+})
+
+
+
+
 let myLibrary = [];
 
 function Book() {
