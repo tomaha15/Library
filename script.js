bff68691a6b47258f7fe03f91885a70f0f473d18
Nikola <tomaha15@gmail.com>
2022-12-08 19:47:20 +0700

    Add transform to new entry element

--- script.js
+++ script.js
@@ -26,10 +26,36 @@ newBook.addEventListener('click', () => {
 
 let myLibrary = [];
 
+myLibrary[0]  = {
+  title:'The Old Man and the Sea',
+  author:'Ernest Hemingway',
+  pages:'127'
+}
+
+myLibrary[1]  = {
+  title:'Alice in Wonderland',
+  author:'Lewis Carroll',
+  pages:'352'
+}
+
+myLibrary[2] = {
+  title:'The Hobbit',
+  author:'J. R. R. Tolkien',
+  pages:'310'
+}
+
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+function test() {
+  myLibrary.forEach((element) => {
+
+  })
+  
+  myLibrary.forEach((element) => console.log(element.title));
 }
